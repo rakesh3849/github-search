3a953613c11722dbacd7dafef26f57e63fd3ba2b
Rakesh kumar <rakeshkumar.pq@gmail.com>
2022-06-28 03:12:42 +0530

    initial commit

---
+++ src/components/Charts/ExampleChart.js
@@ -0,0 +1,58 @@
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+
+
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column3D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column3D, FusionTheme);
+
+// STEP 2 - Chart Data
+
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+
+        const chartConfigs = {
+            type: "pie3d", // The chart type
+            width: "400", // Width of the chart
+            height: "400", // Height of the chart
+            dataFormat: "json", // Data type
+            dataSource: {
+                // Chart Configuration
+                chart: {
+                    //Set the chart caption
+                    caption: "Language",
+                    //Set the chart subcaption
+                    subCaption: "",
+                    //Set the x-axis name
+                    xAxisName: "",
+                    //Set the y-axis name
+                    yAxisName: "",
+                    numberSuffix: "",
+                    //Set the theme for your chart
+                    theme: "fusion"
+                },
+                // Chart Data
+                data
+            }
+        };
+        return ( < ReactFC {...chartConfigs }
+                />);
+            }
+            // STEP 4 - Creating the DOM element to pass the react-fusioncharts component
+
+
+        export default ChartComponent;
